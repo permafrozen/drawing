4e04beb13194a47cdfa0065b7353c67c53c31ae6
Permafrozen <m8tt8o@gmail.com>
2025-05-15 20:48:42 +0200

    Added creation script; template and drawing dirs

---
+++ src/newday.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Simple script to create and open a new drawing for each day.
+# dd_mm_yyyy
+
+date=$(date +%d_%m_%Y)
+time="[$(date +%H:%M:%S)]:"
+name="KRITA_${date}.kra"
+path="./drawings/${name}"
+template="./templates/_TEMPLATE.kra"
+hyprland=0
+krita=0
+
+if [[ $(hyprctl version) != 0 ]]; then
+    hyprland=1
+    echo "$time HYPRLAND DETECTED $hyprland"
+fi
+
+if [[ $(krita -v) != 0 ]]; then
+    krita=1
+    echo "$time KRITA DETECTED $krita"
+fi
+
+open_krita() {
+    if hyprland; then
+        echo "HYPRCTL DISPATCHING KRITA ..."
+        hyprctl dispatch exec krita "$path"
+    elif krita; then
+        echo "$time OPENING KRITA FILE WITH KRITA ..."
+        krita "$path" &
+    else
+        echo "$time OPENING WITH DEFAULT COMMAND ..."
+        open "$path" &
+    fi
+}
+
+if ! test -e ./drawings/"$name"; then
+    cp $template "$path"
+    echo "$time FILE CREATED SUCCESSFULLY: $name"
+    open_krita
+
+else
+    echo "$time FILE ALREADY EXISTS: $name"
+    open_krita
+fi
